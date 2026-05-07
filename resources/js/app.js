8a296004ce434b6e5ca91d8e55a48884f4fc38cd
tairyyounes <tairyyounes@gmail.com>
2026-05-07 16:41:27 -0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import StudentDashboard from './components/StudentDashboard.vue'
 
 const appRoot = document.getElementById('app')
 
 if (appRoot) {
     createApp(App).mount(appRoot)
+}
+
+const studentRoot = document.getElementById('student-dashboard')
+
+if (studentRoot) {
+    createApp(StudentDashboard).mount(studentRoot)
 }
