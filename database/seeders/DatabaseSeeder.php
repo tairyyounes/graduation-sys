f4414621789afd696054efcd59182b488cc35d7c
tairyyounes <tairyyounes@gmail.com>
2026-05-16 10:57:38 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(InitialDataSeeder::class);
 
         User::factory()->create([
-            'name' => 'Test User',
+            'full_name' => 'Test User',
             'email' => 'test@example.com',
+            'department_id' => 1,
         ]);
     }
 }
